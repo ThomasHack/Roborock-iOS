3a2e551aac0be26dd53d6c9ee84ea56cf481ce46
Thomas Hack <mail.thomas.hack@gmail.com>
2021-05-08 14:12:00 +0200

    Initial Commit

---
+++ Roborock/RoborockApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoborockApp.swift
+//  Roborock
+//
+//  Created by Thomas Hack on 08.05.21.
+//
+
+import SwiftUI
+
+@main
+struct RoborockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
