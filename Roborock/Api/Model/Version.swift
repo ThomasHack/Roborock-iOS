fba1aaf7e8300eb860574a1ac6477a790c0b53a0
Thomas Hack <thack@deloitte.de>
2021-06-29 18:04:41 +0200

    +/- map data

---
+++ Roborock/Api/Model/Version.swift
@@ -0,0 +1,15 @@
+//
+//  MapData.swift
+//  Roborock
+//
+//  Created by Hack, Thomas on 29.06.21.
+//
+
+import Foundation
+
+extension MapData {
+    struct Version {
+        var major: Int
+        var minor: Int
+    }
+}
